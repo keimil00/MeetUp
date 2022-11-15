9b01c4c1a55dd60285475bb3d6f26ac821841705
keimil00 <keimil00@gmail.com>
2022-11-15 21:22:45 +0100

    Basic template of login screen

--- app/src/main/java/com/example/meetup/MainActivity.kt
+++ app/src/main/java/com/example/meetup/MainActivity.kt
@@ -1,54 +1,97 @@
 package com.example.meetup
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.meetup.ui.theme.MeetUpTheme
 
-
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        var username: String = "";
+        var password: String = "";
         super.onCreate(savedInstanceState)
         setContent {
             MeetUpTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = "Hello")
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(text = "Material 3")
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    ) { values ->
+                        LazyColumn(contentPadding = values) {
+                            items(1) {
+                                Card(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxWidth(),
+                                    colors = CardDefaults.cardColors(
+                                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+
+                                        ),
+                                    shape = MaterialTheme.shapes.large
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(16.dp)
+                                    ) {
+                                        TextField(
+                                            value = username,
+                                            onValueChange = { username = it },
+                                            label = { Text("Username") },
+                                            singleLine = true,
+                                            placeholder = { Text("Enter your username") },
+                                        )
+                                        Spacer(modifier = Modifier.height(8.dp))
+
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                    }
+                                    Column(
+                                        modifier = Modifier.padding(16.dp)
+                                    ) {
+                                        TextField(
+                                            value = password,
+                                            onValueChange = { password = it },
+                                            label = { Text("Password") },
+                                            singleLine = true,
+                                            placeholder = { Text("Enter your password") },
+                                        )
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                        Button(
+                                            onClick = { /*TODO*/ },
+                                            contentPadding = ButtonDefaults.TextButtonContentPadding,
+                                        ) {
+                                            Text(text = "Login")
+                                        }
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-
-
             }
-        }
-    }
-
-
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun Greeting(name: String) {
-        Scaffold (
-            topBar = { TopAppBar(title = { Text(text = name)}) }
-        ){}
-    }
-
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Greeting(name = "Hello")
         }
     }
 }
--- app/src/main/java/com/example/meetup/ui/theme/Shape.kt
+++ app/src/main/java/com/example/meetup/ui/theme/Shape.kt
@@ -1,11 +1,12 @@
 package com.example.meetup.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
+
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    small = RoundedCornerShape(20.dp),
+    medium = RoundedCornerShape(20.dp),
+    large = RoundedCornerShape(20.dp)
 )
