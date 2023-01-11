6220f162381fe3aed3f525f5ff495a950ba19fa1
Szymon-SR <srszymonsr@gmail.com>
2023-01-11 13:38:02 +0100

    Fix marker size on map

--- app/src/main/java/com/example/meetup/location/MarkerManager.kt
+++ app/src/main/java/com/example/meetup/location/MarkerManager.kt
@@ -3,6 +3,7 @@ package com.example.meetup.location
 import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.ScaleDrawable
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.content.ContextCompat
 import com.example.meetup.R
@@ -15,14 +16,17 @@ import org.osmdroid.views.overlay.Marker
 class MarkerManager {
     companion object {
         private fun prepareDrawable(context: Context, colorString: String): Drawable? {
-            val image = ContextCompat.getDrawable(context, R.drawable.custom_marker)
+            val image = ContextCompat.getDrawable(context, R.drawable.custom_marker40)
 
             val colorARGB: Int = ColorPalette.Primary.get(colorString.toInt()).toArgb()
 
             if (image != null) {
                 image.setColorFilter(colorARGB, PorterDuff.Mode.SRC_ATOP)
+//                val scaleDrawable = ScaleDrawable(image, 0, 10.0F, 10.0F).drawable
+//                image.setBounds(0, 0, 10, 10)
+                return image
             }
-            return image
+            return null
         }
 
         fun createMarker(
